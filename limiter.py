cfe9f4fa1043c27cf5d47e7f9c5a66c6754dc31c
nasonovandrey <andrey.m.nasonov@gmail.com>
2022-03-19 21:10:38 +0300

    Initial commit

---
+++ limiter.py
@@ -0,0 +1,18 @@
+from time import time
+from collections import deque
+
+class limiter:
+    def __init__(self, number_of_calls, limit):
+        self.calls = deque([-float('inf')]*number_of_calls,number_of_calls)
+        self.limit = limit
+
+    def __call__(self, function):
+        def wrapper():
+            current_time = time()
+            if current_time-self.calls[0]<self.limit:
+                print(current_time, 'skipping')
+            else:
+                function()
+                self.calls.append(current_time)
+        return wrapper
+
